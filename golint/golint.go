18ed1d36972e7326562a79cd6836d80efb9f921c
John Weldon <johnweldon4@gmail.com>
2014-05-08 11:32:22 -0700

    reduce confidence of #Initialisms slightly to filter out ID vs. Id type warnings

--- golint/golint.go
+++ golint/golint.go
@@ -15,7 +15,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/golang/lint"
+	"github.com/johnweldon/lint"
 )
 
 var minConfidence = flag.Float64("min_confidence", 0.8, "minimum confidence of a problem to print it")
