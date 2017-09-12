c3b1f712978acc170688ad48e06b4dc3ac536ce5
Kanitkorn S <k.sujautra@gmail.com>
2017-09-12 18:58:07 +0700

    Remove `-vendor` flag from the command

--- golint/golint.go
+++ golint/golint.go
@@ -22,7 +22,6 @@ import (
 var (
 	minConfidence = flag.Float64("min_confidence", 0.8, "minimum confidence of a problem to print it")
 	setExitStatus = flag.Bool("set_exit_status", false, "set exit status to 1 if any issues are found")
-	checkVendor   = flag.Bool("vendor", false, "include packages in vendor dirs when expanding ...")
 	suggestions   int
 )
 
@@ -52,7 +51,7 @@ func main() {
 			if strings.HasSuffix(arg, "/...") && isDir(arg[:len(arg)-len("/...")]) {
 				dirsRun = 1
 				for _, dirname := range allPackagesInFS(arg) {
-					if !*checkVendor && strings.Contains("/" + dirname, "/vendor/") {
+					if strings.Contains("/"+dirname, "/vendor/") {
 						continue
 					}
 					args = append(args, dirname)
