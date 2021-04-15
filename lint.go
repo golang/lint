5a876292fb6b02d95308973a9fd90f93b8736e9d
Josh Soref <jsoref@users.noreply.github.com>
2021-04-14 23:25:20 -0400

    lint: fix spelling of constrained
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- lint.go
+++ lint.go
@@ -666,7 +666,7 @@ func (f *file) lintNames() {
 			}
 		case *ast.InterfaceType:
 			// Do not check interface method names.
-			// They are often constrainted by the method names of concrete types.
+			// They are often constrained by the method names of concrete types.
 			for _, x := range v.Methods.List {
 				ft, ok := x.Type.(*ast.FuncType)
 				if !ok { // might be an embedded interface name
